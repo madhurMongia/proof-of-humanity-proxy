72a91797dbea58bdb44b0941b3616120bee1bb54
madhurMongia <madhurrajput1010@gmail.com>
2025-04-08 22:57:16 +0530

    improved comments

--- contracts/interfaces/ICoreMembersGroup.sol
+++ contracts/interfaces/ICoreMembersGroup.sol
@@ -6,10 +6,5 @@ pragma solidity >=0.8.28;
  * @dev Interface for the Circles Core Members Group contract.
  */
 interface ICoreMembersGroup {
-    /**
-     * @dev Adds trust connections for a batch of users with specified conditions
-     * @param _coreMembers Array of users to trust
-     * @param _expiry Expiration timestamp for the trust (0 to untrust)
-     */
     function trustBatchWithConditions(address[] memory _coreMembers, uint96 _expiry) external;
 }
